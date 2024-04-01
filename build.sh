863a6e0caa4af7421868ebcfd0c63e406a436d2c
Ryan Bai <ryanbai1412@gmail.com>
2024-04-01 13:05:53 -0700

    read year from html file

--- build.sh
+++ build.sh
@@ -6,12 +6,16 @@ mkdir public
 cp -r static/* public
 rm -rf public/ioi-yyyy
 
-for d in ioi-2017 ioi-2019 ioi-2020 ioi-2021 ioi-2022 ioi-2023; do
+for y in 2017 2019 2020 2021 2022 2023; do
+  d=ioi-$y
+
   echo "building $d..."
 
   mkdir public/$d
   cp -r static/ioi-yyyy/* public/$d/
   cp -r $d/* public/$d/
+
+  sed -i s/%IOI-YEAR-e9ea67264dbf2995366f53af646bb20727e41a8b4be912cad2fcd3da24f79a77%/$y/ public/$d/index.html
 
   if [ -f public/$d/patch.sh ]; then
     ( cd public/$d && ./patch.sh )
