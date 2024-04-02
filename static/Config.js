fe75fe70dd899ae6e5586a483885284e19c972b2
Ryan Bai <ryanbai1412@gmail.com>
2024-04-02 12:50:10 -0700

    add complete flag set

--- static/Config.js
+++ static/Config.js
@@ -70,15 +70,13 @@ var Config = new function () {
 
     self.get_flag_url = function (t_key) {
         // 2nd teams
-        if (t_key.charAt(t_key.length - 1) === "2") {
-            t_key = t_key.slice(0, t_key.length - 1);
-        }
+        t_key = t_key.slice(0, 3);
         // the ioi2017 iran team 2 is IRI for some reason
         if (t_key === "IRI") {
             t_key = "IRN";
         }
 
-        return "../flags/" + t_key;
+        return "../flags/" + t_key + ".png";
     };
 
     self.get_face_url = function (u_key) {
---
+++ static/flags/README.md
@@ -0,0 +1,2 @@
+These are the same set of flags used in the [IOI 2023](https://github.com/ioi-2023/ioi-2023.github.io/tree/main/flags) scoreboard.
+The flags are downloaded from https://flagpedia.net/download/icons.
