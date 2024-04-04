8c2eddcd45482e358ce3e9ea4134170598ef10c4
skittles1412 <ryanbai1412@gmail.com>
2024-04-04 01:53:32 -0700

    refactor to vite (#10)

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const years = [2017, 2019, 2020, 2021, 2022, 2023];
+
+export default defineConfig({
+    root: resolve(__dirname, "src"),
+    resolve: {
+        preserveSymlinks: true
+    },
+    build: {
+        emptyOutDir: true,
+        outDir: resolve(__dirname, "dist"),
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/index.html"),
+                ...Object.fromEntries(years.map(y => [`ioi-${y}`, resolve(__dirname, `src/ioi-${y}/index.html`)])),
+            },
+        },
+    },
+});
